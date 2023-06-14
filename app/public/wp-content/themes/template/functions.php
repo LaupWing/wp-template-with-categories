7556aa7c53284687f8f9d6a855b579972d21d32b
LaupWing <loc_strict@hotmail.com>
2023-06-14 16:42:35 +0200

    update

--- app/public/wp-content/themes/template/functions.php
+++ app/public/wp-content/themes/template/functions.php
@@ -1,1 +1,14 @@
 <?php
+
+function loadAssets(){
+}
+
+add_action("wp_enqueue_scripts", "loadAssets");
+
+function addSupport(){
+   register_nav_menu("headerMenuLocation", "Header Menu Location");
+   // add_theme_support("title-tag");
+   // add_theme_support("post-thumbnails");
+}
+
+add_action("after_setup_theme", "addSupport");
