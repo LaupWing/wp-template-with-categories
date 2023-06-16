594873c91359fb53a83b4abc9ad98480f714082b
LaupWing <loc_strict@hotmail.com>
2023-06-16 17:11:58 +0200

    copied tempaltes

---
+++ app/public/wp-content/themes/template/templates/icons/arrowRightUp.php
@@ -0,0 +1,14 @@
+<?php 
+   $custom_arrow_class = isset($args["custom_class"]) ? $args["custom_class"] : "w-6 h-6";
+?>
+
+<svg 
+   xmlns="http://www.w3.org/2000/svg" 
+   fill="none" 
+   viewBox="0 0 24 24" 
+   stroke-width="1.5" 
+   stroke="currentColor" 
+   class="<?php echo $custom_arrow_class ?>"
+>
+  <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" />
+</svg>
