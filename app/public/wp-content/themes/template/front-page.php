1c89c3895bb3b4b24fb34237869c2893bf9b7915
LaupWing <loc_strict@hotmail.com>
2023-06-16 19:28:58 +0200

    update

--- app/public/wp-content/themes/template/front-page.php
+++ app/public/wp-content/themes/template/front-page.php
@@ -2,8 +2,12 @@
    get_header()
 ?>
    <main>
-      <div class="h-screen-minus-nav bg-slate-50 w-full">
-
+      <div class="h-screen-minus-nav bg-orange-300 w-full flex">
+         <div class="container mx-auto flex-1 flex items-center">
+            <div class="bg-orange-200 px-4 py-2 rounded">
+               <h2>Here comes the cta</h2>
+            </div>
+         </div>
       </div>
    </main>
 <?php 
