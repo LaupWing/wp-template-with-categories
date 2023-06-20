cf8b96511899d5b4c39d2fee28c809a2b5ee4c15
LaupWing <loc_strict@hotmail.com>
2023-06-20 12:52:05 +0200

    radio finished

---
+++ app/public/wp-content/themes/template/src/modules/FormSteps/Radio.js
@@ -0,0 +1,42 @@
+export default ({
+   name,
+   values,
+   label
+}) => {
+   return (
+      <div className="flex flex-col my-4 text-lg">
+         <p className="font-semibold">{label}</p>
+         <div className="flex gap-4 mt-1">
+            {values.map(value => (
+               <Option 
+                  value={value}
+                  name={name}
+               />
+            ))}
+         </div>
+      </div>
+   )
+}
+
+const Option = ({
+   value,
+   name
+}) => {
+   const id = `${name}_${value}` 
+   return (
+      <div className="flex gap-2 items-center text-2">
+         <input 
+            type="radio" 
+            name={name} 
+            id={id} 
+            value={value}
+         />
+         <label 
+            htmlFor={id}
+            className="capitalize"
+         >
+            {value}
+         </label>
+      </div>
+   )
+}
