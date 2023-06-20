671308810a6f63006cb2bea1c8d67e9c0ac62832
LaupWing <loc_strict@hotmail.com>
2023-06-20 13:56:37 +0200

    update

--- app/public/wp-content/themes/template/src/modules/FormSteps/Text.js
+++ app/public/wp-content/themes/template/src/modules/FormSteps/Text.js
@@ -1,13 +1,16 @@
 export default ({
    name,
    value,
-   label
+   label,
+   type = "text",
+   ...props
 }) => {
    return (
       <div className="flex flex-col text-lg items-start">
          <p className="font-semibold">{label}</p>
          <input 
-            type="text" 
+            type={type} 
+            {...props}
             className="bg-gray-100 rounded placeholder-gray-900 w-full"
             name={name} 
             id={name} 
