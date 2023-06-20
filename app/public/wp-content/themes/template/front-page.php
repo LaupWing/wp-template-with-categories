82655c51620f9ee1492da45e9529ab7109b9af12
LaupWing <loc_strict@hotmail.com>
2023-06-20 10:23:17 +0200

    update

--- app/public/wp-content/themes/template/front-page.php
+++ app/public/wp-content/themes/template/front-page.php
@@ -8,7 +8,7 @@
       >
          <section class="container mx-auto flex-1 flex flex-col bg-main my-10 rounded shadow p-10">
             <div class="mb-4">
-               <h2 class="text-2xl">Binnen 1 minuut uw project berekenen?</h2>
+               <h2 class="text-3xl">Binnen 1 minuut uw project berekenen?</h2>
             </div>
             <div class="flex w-full gap-2">
                <div class="h-2 bg-accent-1 flex-1"></div>
@@ -21,10 +21,17 @@
             </div>
             <form 
                action=""
-               class="p-4 flex-1 flex"
+               class="p-4 pt-6 flex-1 flex"
             >
-               <div class="flex-1">
-
+               <div class="flex-1 flex items-center flex-col">
+                  <h3 class="text-2xl">Betreft het nieuwbouw of een renovatie woning?</h3>
+                  <div class="flex">
+                     <img 
+                        src="<?php echo get_theme_file_uri("/images/placeholder_image.png") ?>" 
+                        alt="Nieuwbouw"
+                        class="w-48 h-48"
+                     >
+                  </div>
                </div>
             </form>
          </section>
