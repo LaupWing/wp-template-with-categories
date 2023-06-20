c81eeee4fa642221c9d4bdd4d96c9921e2d6c4b3
LaupWing <loc_strict@hotmail.com>
2023-06-20 09:17:58 +0200

    udpate

--- app/public/wp-content/themes/template/header.php
+++ app/public/wp-content/themes/template/header.php
@@ -8,13 +8,16 @@
 <body <?php body_class() ?>>
    <header class="items-center h-nav bg-orange-100 flex justify-center max-w-6xl mx-auto">
       <div class="container flex justify-between items-center">
-         <div class="flex items-end">
+         
             <h1 class="font-bold mr-8 text-xl font-cursive">
                <img 
                   src="<?php echo get_theme_file_uri("/images/logo.png") ?>" 
                   alt="logo"
+                  class="w-56"
                >
             </h1>
+            
+         <div class="flex items-center">
             <nav>
                <?php
                   wp_nav_menu([
@@ -22,8 +25,6 @@
                   ]);
                ?>
             </nav>
-         </div>
-         <div>
             <div class="w-[34px] h-[34px] text-white bg-black rounded-full flex items-center justify-center">
                <?php  get_template_part("templates/icons/search")?>
             </div>
