6921572462097e005d4d2d280a40d46cc38c9215
LaupWing <loc_strict@hotmail.com>
2023-06-20 09:37:46 +0200

    update

--- app/public/wp-content/themes/template/front-page.php
+++ app/public/wp-content/themes/template/front-page.php
@@ -2,7 +2,10 @@
    get_header()
 ?>
    <main>
-      <div class="h-screen-minus-nav bg-orange-300 w-full flex">
+      <div 
+         style="background: url('<?php echo get_theme_file_uri("/images/placeholder_bg.jpg") ?>'); background-size: cover;"
+         class="h-screen-minus-nav bg-main w-full flex"
+      >
          <div class="container mx-auto flex-1 flex items-center">
             <div class="bg-orange-200 px-4 py-2 rounded">
                <h2>Here comes the cta</h2>
