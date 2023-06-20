7152456a06e6f2ca6f10f40360b38e333357e566
LaupWing <loc_strict@hotmail.com>
2023-06-20 09:12:16 +0200

    added the logo

--- app/public/wp-content/themes/template/header.php
+++ app/public/wp-content/themes/template/header.php
@@ -9,7 +9,12 @@
    <header class="items-center h-nav bg-orange-100 flex justify-center max-w-6xl mx-auto">
       <div class="container flex justify-between items-center">
          <div class="flex items-end">
-            <h1 class="font-bold mr-8 text-xl font-cursive">Template</h1>
+            <h1 class="font-bold mr-8 text-xl font-cursive">
+               <img 
+                  src="<?php echo get_theme_file_uri("/images/logo.png") ?>" 
+                  alt="logo"
+               >
+            </h1>
             <nav>
                <?php
                   wp_nav_menu([
