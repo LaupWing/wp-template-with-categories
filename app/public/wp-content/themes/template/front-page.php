e45ca13e205559a71887a63a6534b97a32fa7c0d
LaupWing <loc_strict@hotmail.com>
2023-06-21 09:03:16 +0200

    udpate

--- app/public/wp-content/themes/template/front-page.php
+++ app/public/wp-content/themes/template/front-page.php
@@ -8,6 +8,22 @@
       >
          <div class="container mx-auto flex" id="form-steps"></div>
       </div>
+      <div class="bg-accent-1 w-full py-4">
+         <div class="container text-center mx-auto text-main divide-x-2 divide-main/30 flex gap-2">
+            <div class="flex-1 py-8">
+               Hoogste kwaliteit, beste prijs
+            </div>
+            <div class="flex-1 py-8">
+               Hoogste kwaliteit, beste prijs
+            </div>
+            <div class="flex-1 py-8">
+               Hoogste kwaliteit, beste prijs
+            </div>
+            <div class="flex-1 py-8">
+               Hoogste kwaliteit, beste prijs
+            </div>
+         </div>
+      </div>
    </main>
 <?php 
    get_footer()
