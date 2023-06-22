3fdc730304f153f2c7f04d7af17994dc7c984f53
LaupWing <loc_strict@hotmail.com>
2023-06-22 19:33:01 +0200

    update

--- app/public/wp-content/themes/template/page-onze-diensten.php
+++ app/public/wp-content/themes/template/page-onze-diensten.php
@@ -17,15 +17,25 @@
                   while($query->have_posts()){
                      $query->the_post();
             ?>
-               <li class="bg-main aspect-[4/6] rounded-md p-6">
+               <li class="bg-main aspect-[4/6] rounded-md p-6 flex flex-col">
                   <img 
                      src="<?php echo get_the_post_thumbnail_url() ?>" 
                      alt="<?php the_title() ?> foto"
                      class="aspect-square object-cover"
                   >
-                  <h3 class="text-accent-1 mt-4 text-lg font-bold">
+                  <h3 class="text-accent-1 mt-2 font-bold">
                      <?php the_title() ?>
                   </h3>
+                  <p class="text-xs">
+                     <?php echo wp_trim_words(wp_strip_all_tags(get_the_content()), 20, "..."); ?>
+                  </p>
+
+                  <a 
+                     href="<?php the_permalink() ?>"
+                     class="text-xs uppercase font-bold mt-auto bg-accent-2 py-1 px-3 rounded text-main mr-auto"
+                  >
+                     Lees Meer
+                  </a>
                </li>
             <?php
                   }
