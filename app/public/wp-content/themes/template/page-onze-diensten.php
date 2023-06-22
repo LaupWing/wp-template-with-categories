2d8b1e2be62dd632313d6691d2d75d7f975d6507
LaupWing <loc_strict@hotmail.com>
2023-06-22 07:48:12 +0200

    min h screen

--- app/public/wp-content/themes/template/page-onze-diensten.php
+++ app/public/wp-content/themes/template/page-onze-diensten.php
@@ -1,9 +1,8 @@
 <?php 
    get_header()
 ?>
-   <div class="container mx-auto">
+   <div class="container mx-auto min-h-screen-minus-nav">
       <h2 class="text-3xl text-accent-2 font-bold">Onze diensten</h2>
-      
    </div>
 <?php 
    get_footer()
