2ce57a617c3c2adb90182aa0777db2b19aaaec41
LaupWing <loc_strict@hotmail.com>
2023-06-22 08:01:41 +0200

    udpate

--- app/public/wp-content/themes/template/page-onze-diensten.php
+++ app/public/wp-content/themes/template/page-onze-diensten.php
@@ -1,8 +1,10 @@
 <?php 
    get_header()
 ?>
-   <div class="container mx-auto min-h-screen-minus-nav">
-      <h2 class="text-3xl text-accent-2 font-bold">Onze diensten</h2>
+   <div class="min-h-screen-minus-nav bg-accent-1/40">
+      <div class="container mx-auto pt-10">
+         <h2 class="text-3xl text-accent-2 font-bold">Onze diensten</h2>
+      </div>
    </div>
 <?php 
    get_footer()
