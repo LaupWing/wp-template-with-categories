74d74bbea4bb2b8408343fe74cb44e5647610a77
LaupWing <loc_strict@hotmail.com>
2023-06-27 14:31:07 +0200

    update

--- app/public/wp-content/themes/template/single.php
+++ app/public/wp-content/themes/template/single.php
@@ -1,12 +1,15 @@
 <?php 
    get_header()
 ?>
-   <div 
-      style="background: url('<?php echo get_the_post_thumbnail_url() ?>'); background-size: cover;"
-      class="h-[50vh] bg-main w-full flex"
-   >
-      <div class="container mx-auto flex items-center justify-center">
-         <h2 class="text-2xl backdrop-blur bg-main/60 font-bold px-2 py-1 rounded text-accent-2"><?php the_title() ?></h2>
+   <div class="min-h-screen-minus-nav flex flex-col">
+      <div 
+         style="background: url('<?php echo get_the_post_thumbnail_url() ?>'); background-size: cover;"
+         class="h-full bg-orange-50 flex-1 max-h-[300px] flex items-center justify-center"
+      >
+         <h2 class="text-3xl text-accent-2 font-bold bg-main/60 p-4 backdrop-blur rounded"><?php the_title() ?></h2>    
+      </div>
+      <div class="container mx-auto mt-6">
+         <?php the_content() ?>
       </div>
    </div>
 <?php 
