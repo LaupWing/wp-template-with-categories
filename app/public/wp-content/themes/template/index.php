5739f06d9c127ff0e87c960ec5b8d53146a41851
LaupWing <loc_strict@hotmail.com>
2023-06-27 13:42:41 +0200

    udpate

--- app/public/wp-content/themes/template/index.php
+++ app/public/wp-content/themes/template/index.php
@@ -1,1 +1,9 @@
-Blog page
+<?php 
+   get_header()
+?>
+   <div class="container mx-auto min-h-screen-minus-nav bg-accent-1/10">
+      <h2 class="text-3xl text-accent-2 font-bold">Portfolio</h2>    
+   </div>
+<?php 
+   get_footer()
+?>
