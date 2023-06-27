ecd585c31602c79a96ef482ace86472d99ea7ec1
LaupWing <loc_strict@hotmail.com>
2023-06-27 14:28:24 +0200

    udpate

--- app/public/wp-content/themes/template/page.php
+++ app/public/wp-content/themes/template/page.php
@@ -6,9 +6,11 @@
          style="background: url('<?php echo get_the_post_thumbnail_url() ?>'); background-size: cover;"
          class="h-full bg-orange-50 flex-1 max-h-[300px] flex items-center justify-center"
       >
-         <h2 class="text-3xl text-accent-2 font-bold bg-main/60"><?php the_title() ?></h2>    
+         <h2 class="text-3xl text-accent-2 font-bold bg-main/60 p-4 backdrop-blur rounded"><?php the_title() ?></h2>    
       </div>
-
+      <div class="container mx-auto mt-6">
+         <?php the_content() ?>
+      </div>
    </div>
 <?php 
    get_footer()
