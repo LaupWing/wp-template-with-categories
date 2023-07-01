758c1e3068160d91c66dfa8d111a338e44839585
LaupWing <loc_strict@hotmail.com>
2023-07-01 08:12:23 +0200

    udpate

--- app/public/wp-content/themes/template/build/index.asset.php
+++ app/public/wp-content/themes/template/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-element'), 'version' => '244225655f357a537122');
+<?php return array('dependencies' => array('react', 'wp-element'), 'version' => '84fa5a9376211babc14a');
