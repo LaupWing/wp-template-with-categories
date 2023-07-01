a31508971bf051ebecfdff461e8bb59718bbf4eb
LaupWing <loc_strict@hotmail.com>
2023-07-01 16:16:47 +0200

    udpate

--- app/public/wp-content/themes/template/front-page.php
+++ app/public/wp-content/themes/template/front-page.php
@@ -56,7 +56,7 @@
                <div class="grid grid-cols-2 relative">
                   <?php if($counter%2 === 0) {?>
                      <div class="flex flex-col my-auto items-start z-50">
-                        <h2 class="text-2xl mb-2 border-b-4 border-accent-1">
+                        <h2 class="sm:text-2xl text-xl mb-2 border-b-4 border-accent-1">
                            <?php the_title() ?>
                         </h2>
                         <p class="text-sm sm:text-base">
@@ -64,7 +64,7 @@
                         </p>
                         <a class="text-xs mt-4 uppercase font-bold tracking-wider text-main px-2 py-1 bg-accent-2 rounded" href="<?php the_permalink() ?>">Lees Meer</a>
                      </div>
-                     <div class="m-6 sm:relative absolute inset-0">
+                     <div class="m-6 sm:relative absolute right-0 w-2/3">
                         <?php the_post_thumbnail(); ?>
                         <span class="absolute bottom-2 left-2 bg-main/70 backdrop-blur-sm text-accent-2 px-2 uppercase text-sm rounded font-bold">
                            <?php echo get_the_date() ?>
