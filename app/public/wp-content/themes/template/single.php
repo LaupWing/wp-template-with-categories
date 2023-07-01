759e53532b3c4c6eb317ba7ee6ffd88d02495f60
LaupWing <loc_strict@hotmail.com>
2023-07-01 19:55:07 +0200

    update

--- app/public/wp-content/themes/template/single.php
+++ app/public/wp-content/themes/template/single.php
@@ -4,7 +4,7 @@
    <div class="min-h-screen-minus-nav flex flex-col">
       <div 
          style="background: url('<?php echo get_the_post_thumbnail_url() ?>'); background-size: cover;"
-         class="h-full flex-shrink-0 flex-1 max-h-[50vh] flex items-center justify-center min-h-0"
+         class="h-[50vh] flex-shrink-0 flex items-center justify-center min-h-0"
       >
          <h2 class="text-3xl text-accent-2 font-bold bg-main/60 p-4 backdrop-blur rounded"><?php the_title() ?></h2>    
       </div>
