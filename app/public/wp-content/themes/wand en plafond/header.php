34c803a8eb08cdb73fee4881876df18b61a5dc92
LaupWing <loc_strict@hotmail.com>
2023-07-01 17:15:19 +0200

    update

---
+++ app/public/wp-content/themes/wand en plafond/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html <?php language_attributes() ?>>
+<head>
+   <meta charset="<?php bloginfo("charset") ?>">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <?php wp_head() ?>
+</head>
+<body <?php body_class() ?>>
+   <header class="items-center h-nav bg-main flex sticky top-0 z-[100]">
+      <div class="container flex justify-between items-center mx-auto">
+         <h1 class="font-bold mr-8 text-xl font-cursive">
+            <img 
+               src="<?php echo get_theme_file_uri("/images/logo.png") ?>" 
+               alt="logo"
+               class="w-56"
+            >
+         </h1>
+         <div class="flex sm:hidden">
+            <?php get_template_part(
+               "templates/icons/bars",
+               null,
+               array(
+                  "custom_class" => "mr-4 w-6 h-6"
+               )
+            ) ?>
+         </div>
+         <div class="sm:flex items-center hidden">
+            <nav>
+               <?php
+                  wp_nav_menu([
+                     "theme_location" => "headerMenuLocation"
+                  ]);
+               ?>
+            </nav>
+         </div>
+      </div>
+   </header>
+   <main>
---
+++ app/public/wp-content/themes/wand en plafond/single.php
@@ -0,0 +1,17 @@
+<?php 
+   get_header()
+?>
+   <div class="min-h-screen-minus-nav flex flex-col">
+      <div 
+         style="background: url('<?php echo get_the_post_thumbnail_url() ?>'); background-size: cover;"
+         class="h-full bg-orange-50 flex-1 max-h-[300px] flex items-center justify-center"
+      >
+         <h2 class="text-3xl text-accent-2 font-bold bg-main/60 p-4 backdrop-blur rounded"><?php the_title() ?></h2>    
+      </div>
+      <div class="container mx-auto mt-6">
+         <?php the_content() ?>
+      </div>
+   </div>
+<?php 
+   get_footer()
+?>
