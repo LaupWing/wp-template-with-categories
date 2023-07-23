fd9c00670ef1d986003c36400f1daff6baf00b82
LaupWing <loc_strict@hotmail.com>
2023-07-23 15:39:02 +0200

    udpated

--- app/public/wp-content/themes/template/src/modules/FormSteps/Steps/Step6.js
+++ app/public/wp-content/themes/template/src/modules/FormSteps/Steps/Step6.js
@@ -12,39 +12,90 @@ export default ({
                   label={"E-mailadres *"}
                   name={"email"}
                   value={formData.step6?.email || ""}
+                  onChange={(e) => {
+                     update({
+                        step6: {
+                           ...formData.step6,
+                           email: e.target.value
+                        }
+                     })
+                  }}
                   className="col-span-2 sm:col-span-1"
                   type="email"
                />
                <Text
                   label={"Telefoonnummer *"}
                   name={"telefoonnummer"}
-                  value={""}
+                  value={formData.step6?.phonenumber || ""}
+                  onChange={(e) => {
+                     update({
+                        step6: {
+                           ...formData.step6,
+                           phonenumber: e.target.value
+                        }
+                     })
+                  }}
                   className="col-span-2 sm:col-span-1"
-                  type="email"
+                  type="text"
                />
                <Text
                   label={"Voornaam *"}
                   name={"voornaam"}
                   className="col-span-2 sm:col-span-1"
-                  value={""}
+                  value={formData.step6?.firstname || ""}
+                  onChange={(e) => {
+                     update({
+                        step6: {
+                           ...formData.step6,
+                           firstname: e.target.value
+                        }
+                     })
+                  }}
+                  type="text"
                />
                <Text
                   label={"Achternaam *"}
                   name={"achternaam"}
                   className="col-span-2 sm:col-span-1"
-                  value={""}
+                  value={formData.step6?.lastname || ""}
+                  onChange={(e) => {
+                     update({
+                        step6: {
+                           ...formData.step6,
+                           lastname: e.target.value
+                        }
+                     })
+                  }}
+                  type="text"
                />
                <Text
                   label={"Opmerkingen/bijzonderheden?"}
                   name={"opmerkingen"}
-                  value={""}
+                  value={formData.step6?.comments || ""}
+                  onChange={(e) => {
+                     update({
+                        step6: {
+                           ...formData.step6,
+                           comments: e.target.value
+                        }
+                     })
+                  }}
+                  type="text"
                   className="col-span-2"
                />
                <Text
                   label={"Wat is de prognose opleverdatum? *"}
                   name={"datum"}
                   type="date"
-                  value={""}
+                  value={formData.step6?.date || ""}
+                  onChange={(e) => {
+                     update({
+                        step6: {
+                           ...formData.step6,
+                           date: e.target.value
+                        }
+                     })
+                  }}
                   className="col-span-2"
                />
             </div>
