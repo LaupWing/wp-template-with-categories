b46ba22b294fa599e7f2ba79b434197029d8e5bd
LaupWing <loc_strict@hotmail.com>
2023-07-23 20:27:35 +0200

    redo

--- app/public/wp-content/themes/export/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-element'), 'version' => 'beb32d570621c699bac5');
--- app/public/wp-content/themes/export/src/index.js
+++
@@ -1,7 +0,0 @@
-
-if(window.location.pathname === "/"){
-   import("./modules/FormSteps")
-}
-import "./modules/Socials"
-import SideNav from "./modules/SideNav"
-new SideNav()
