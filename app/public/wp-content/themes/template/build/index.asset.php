f7a2983484c60e13530dbd64e8b85b9c5ee9f28f
LaupWing <loc_strict@hotmail.com>
2023-07-23 15:41:37 +0200

    updated

--- app/public/wp-content/themes/template/build/index.asset.php
+++ app/public/wp-content/themes/template/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-element'), 'version' => '03ea63a0637d8912f955');
+<?php return array('dependencies' => array('react', 'wp-element'), 'version' => 'c110b467027710ff3afb');
